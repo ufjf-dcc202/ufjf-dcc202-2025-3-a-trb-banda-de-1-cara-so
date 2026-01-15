fcaab893b97598a0eea10a76ef3046961a739c2d
matt-lino <matt.almeidac@gmail.com>
2026-01-14 23:51:48 -0300

    add class board: primeiro grid com js

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-ctx.strokeStyle = "red";
-ctx.strokeRect(100, 100, 100, 100);
+
+
+class Board{
+    constructor (cellWidth,cellHeight,cols, rows){
+        this.cellW = cellWidth;
+        this.cellH = cellHeight;
+        this.rows = rows;
+        this.cols = cols;
+    }
+    drawGrid(){
+    for (let c = 0; c < this.cols; c++){
+        for (let r = 0; r < this.rows; r++){
+            ctx.strokeStyle = "blue";
+            ctx.strokeRect(c*this.cellW, r*this.cellH, this.cellW, this.cellH);
+            //ctx.strokeRect(j*this.width, i*this.height, this.width, this.height);
+        }
+    }
+    }
+    
+}
+
+const teste = new Board(100, 100, 5, 5);
+teste.drawGrid();
+
 
