0f6d8d8f681fd2bd1d60c536af5c41b687c9aae3
matheusac <matt.almeidac@gmail.com>
2026-01-18 00:10:36 -0300

    melhora geração de grid + pontos na tela

--- src/classes/Grid.js
+++ src/classes/Grid.js
@@ -1,27 +1,50 @@
 // classe pra setar um grid
 
 class Grid {
-    constructor(rows, cols, cellSize, canvasHeight, canvasWidth) {
-        this.cellSize = cellSize;
+    constructor(rows, cols, canvasWidth, canvasHeight) {
         this.rows = rows;
         this.cols = cols;
-        this.xOffset = 100; // definir offset pro centro
-        this.yOffset = 100; // 
+
+        this.canvasHeight = canvasHeight;
+        this.canvasWidth = canvasWidth;
+
+        this.cellSize = 0;
+        this.offsetX = 0; // definir offset pro centro
+        this.offsetY = 0; // ou fazer o grid ocupar todo o canvas
+
         // criando um array 2d vazio
         this.gridVertices = Array(this.rows + 1).fill().map(() => Array(this.cols + 1));
         this.mids = Array(this.rows).fill().map(() => Array(this.cols));
+
         // iniciar auto certas funções básicas
-        this.fillCoordinatesArray();
+        this.calculateGrid();
+        this.createGrid();
     }
 
 
-    fillCoordinatesArray() {
+    calculateGrid() {
+        // conta pra centralizar o grid e dimensionar pra caber
+
+        // 
+        const maxWidth = this.canvasWidth / this.cols;
+        const maxHeight = this.canvasHeight / this.rows;
+
+        this.cellSize = Math.min(maxWidth, maxHeight);
+
+        this.gridWidth = this.cols * this.cellSize;
+        this.gridHeight = this.rows * this.cellSize;
+
+        this.offsetX = (this.canvasWidth - this.gridWidth) / 2;
+        this.offsetY = (this.canvasHeight - this.gridHeight) / 2;
+    }
+
+    createGrid() {
         //preenchendo a(s) linhas com coordenadas iniciais
         for (let r = 0; r < this.rows + 1; r++) {
             for (let c = 0; c < this.cols + 1; c++) {
                 this.gridVertices[r][c] = {
-                    x: c * this.cellSize + this.xOffset,
-                    y: r * this.cellSize + this.yOffset
+                    x: c * this.cellSize + this.offsetX,
+                    y: r * this.cellSize + this.offsetY
                 };
             }
         }
@@ -29,8 +52,8 @@ class Grid {
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.cols; c++) {
                 this.mids[r][c] = {
-                    x: (c * this.cellSize) + this.cellSize / 2 + this.xOffset,
-                    y: (r * this.cellSize) + this.cellSize / 2 + this.yOffset,
+                    x: (c * this.cellSize) + this.cellSize / 2 + this.offsetX,
+                    y: (r * this.cellSize) + this.cellSize / 2 + this.offsetY,
                 };
             }
         }
@@ -74,7 +97,19 @@ class Grid {
             ctx.lineTo(this.gridVertices[lastRow][c].x, this.gridVertices[lastRow][c].y);
             ctx.stroke();
         }
+
+        // pra debug, escrever coordenadas
+        ctx.fillStyle = "#666";
+        ctx.font = "12px Arial";
+        ctx.textAlign = "center";
+
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                ctx.fillText(`${r},${c}`, this.mids[r][c].x, this.mids[r][c].y);
+            }
+        }
     }
 }
+
 
 export default Grid;
