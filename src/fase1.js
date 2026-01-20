9b9713fd083570447237271e4b20d82d90fa3755
matheusac <matt.almeidac@gmail.com>
2026-01-20 17:46:59 -0300

    Revert "Merge commit '8aa520d91ea5f219fb25a9f85e7c01f4fd482dfb' into dev"
    This reverts commit bfe2408a85f5f6a61a247ddc0b171c49039a4b74, reversing
    changes made to 9ed7ffbfa87de4292b9020940032749925ba1b88.

---
+++ src/fase1.js
@@ -0,0 +1,5 @@
+/* 
+    1. definir linhas e colunas da grid (pra gerar um canvas de acordo)
+    2. definir ponto de início do player
+    3. 
+*/
--- src/script.js
+++ src/script.js
@@ -1,139 +1,50 @@
+import Temas from "./classes/Temas.js";
+
+
+import Game from "./classes/Game.js";
 import Grid from "./classes/Grid.js";
 import Player from "./classes/Player.js";
 
+let gridRows = 5;
+let gridCols = 5;
 
-const themeDarkween = document.getElementById("darkween");
-const themeMadruga = document.getElementById("madruga");
-const themeLightween = document.getElementById("lightween");
-const themeSwomp = document.getElementById("swomp");
+const tema = new Temas;
+const game = new Game(gridRows, gridCols);
+const ctx = game.ctx;
+const canvasWidth = game.canvasWidth;
+const canvasHeight = game.canvasHeight;
 
-// themes
-themeDarkween.addEventListener('click', () => {
-    document.documentElement.setAttribute('data-theme', 'darkween');
-});
-themeMadruga.addEventListener('click', () => {
-    document.documentElement.setAttribute('data-theme', 'madruga');
-});
-themeLightween.addEventListener('click', () => {
-    document.documentElement.setAttribute('data-theme', 'lightween');
-});
-themeSwomp.addEventListener('click', () => {
-    document.documentElement.setAttribute('data-theme', 'swomp');
-});
+const grid = new Grid(ctx, gridRows, gridCols, canvasWidth, canvasHeight);
+const player = new Player(ctx, grid.mids, grid.gridVertices);
+
+grid.drawCells();
 
 
-const canvas = document.getElementById("jogo");
-const ctx = canvas.getContext("2d");
 
-const screen = Math.min(innerWidth, innerHeight);
+const rotateClockBtn = document.getElementById('rotateClock');
+const rotateAntiBtn = document.getElementById('rotateAnti');
+const forwardBtn = document.getElementById('forward');
+const jumpBtn = document.getElementById('jump');
 
-canvas.width = screen;
-canvas.height = screen;
-
-//if (window.innerWidth / innerHeight > 0.7) {
-//    canvas.width = 800;
-//    canvas.height = 600;
-//} else {
-//    canvas.width = 600;
-//    canvas.height = 800;
-//}
-
-console.log(window.innerHeight / window.innerWidth);
-
-// get buttons
-const rightBtn = document.getElementById('right');
-const leftBtn = document.getElementById("left");
-const upBtn = document.getElementById("up");
-const downBtn = document.getElementById("down");
-
-console.log("oi");
-
-const grid = new Grid(5, 5, canvas.width, canvas.height);
-const player = new Player(grid.mids, grid.gridVertices);
-
-
-//console.log(grid.mids);
-//console.log(grid.gridVertices);
-//const teste = grid.mids.length;
-//console.log(teste);
-
-// separar main menu e fases
-
-// as dimensões estão meio malucas
-
-// fazer player class: usar svg, img ou canvas?
-// fazer colisão de obj
-
-// fazer animações
-
-//function checkCollision(obj1, obj2) {
-//    if (obj1.x = obj2.x)
-//}
-
-
-/* teste de animação
-let x = 10;
-let y = 10;
-let dx = 5;
-let dy = 5;
-let o = 10;
-
-
-// desenha algo
-function ponto(px, py, o) {
-    ctx.fillStyle = `hsl(${o},100%,50%)`;
-    ctx.fillRect(px, py, 30, 30);
-}
-
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    grid.drawGrid(ctx);
-
-    ponto(x, y, o);
-
-    if ((x) < 0) {
-        dx = -dx;
-        o += 10;
-    }
-    if ((x + 30) == canvas.width - 2) {
-        dx = -dx;
-        o += 10;
-    }
-
-    if ((y) < 0) {
-        dy = -dy;
-        o += 10;
-    }
-    if ((y + 30) == canvas.height - 1) {
-        dy = -dy;
-        o += 10;
-    }
-
-    x += dx;
-    y += dy;
-
-    //console.log(y);
-
-    requestAnimationFrame(gameLoop);
+function mover() {
+    rotateClockBtn.addEventListener('click', () => player.rotateClock++);
+    rotateAntiBtn.addEventListener('click', () => player.rotateAnti++);
+    forwardBtn.addEventListener('click', () => player.moves++);
+    jumpBtn.addEventListener('click', () => player.jumps++);
 
 }
-*/
 
-rightBtn.addEventListener('click', () => player.moveRight());
-leftBtn.addEventListener('click', () => player.moveLeft());
-upBtn.addEventListener('click', () => player.moveUp());
-downBtn.addEventListener('click', () => player.moveDown());
+mover();
 
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    grid.drawGrid(ctx);
-    player.drawPlayer(ctx);
+function gameLoop2() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     player.update();
-
-    requestAnimationFrame(gameLoop);
-    console.log("fim");
+    // dps coloca um grid.update()
+    grid.drawGrid();
+    grid.drawCells();
+    grid.drawMidPoints();
+    requestAnimationFrame(gameLoop2);
 }
-// ponto(canvas.width / 2, canvas.height / 2);
 
 
-gameLoop();
+gameLoop2();
